399e46cf4069921393163c428860629d0c6b2dd1
Petri Savolainen <petri@koodaamo.fi>
2018-11-29 12:57:04 +0200

    improve cmdline test coverage

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,7 +1,10 @@
 
-def test_foo_bar(script_runner):
+def test_cmdline_overview(script_runner):
     ret = script_runner.run('tnefparse', '-o', 'tests/examples/body.tnef')
     assert ret.success
     assert "Overview of tests/examples/body.tnef" in ret.stdout
     assert ret.stderr == ''
 
+def test_cmdline_attch_extract(script_runner):
+    ret = script_runner.run('tnefparse', '-a', 'tests/examples/one-file.tnef')
+    assert ret.stderr == 'Successfully wrote 1 files\n'
