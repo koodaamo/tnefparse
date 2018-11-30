7c477e8bcd7efb4ff83e6f2b9e2756a32774eab0
Petri Savolainen <petri@koodaamo.fi>
2018-11-30 08:40:02 +0200

    fix parseFile deprecation

--- tnefparse/__init__.py
+++ tnefparse/__init__.py
@@ -1,7 +1,8 @@
+import warnings
 from .tnef import TNEF, TNEFAttachment, TNEFObject
 
 
 def parseFile(fileobj):
     "a convenience function that returns a TNEF object"
-    raise PendingDeprecationWarning("parseFile will be deprecated after 1.3")
+    warnings.warn("parseFile will be deprecated after 1.3", DeprecationWarning)
     return TNEF(fileobj.read())
